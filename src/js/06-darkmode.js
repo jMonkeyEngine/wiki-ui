60831df32ba39f695a7aaa17ee945ad7301c84f5
Davis Rollman <drollma@hotmail.com>
2020-06-19 17:22:36 -0400

    Added jmonkey theme

---
+++ src/js/06-darkmode.js
@@ -0,0 +1,25 @@
+var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
+var currentTheme = window.localStorage.getItem('theme')
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme)
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true
+  }
+} else {
+  //set dark by default
+  document.documentElement.setAttribute('data-theme', 'light')
+  toggleSwitch.checked = false
+}
+
+function switchTheme (e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    window.localStorage.setItem('theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+    window.localStorage.setItem('theme', 'light')
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false)
